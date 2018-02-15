c68ea4c04557247055ac60bf1587475990e36590
ollepeter <mr.peterolle@gmail.com>
2018-02-15 12:59:56 +0100

    Initialize Application

---
+++ src/main/java/com/codecool/happiness_jar/hj_user_service/HjUserService.java
@@ -0,0 +1,12 @@
+package com.codecool.happiness_jar.hj_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HjUserService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HjUserService.class, args);
+	}
+}
