0c39ca8b007b70b8a85e0e4e8b40eaa67283a954
ollepeter <mr.peterolle@gmail.com>
2018-02-15 16:16:06 +0100

    Create "UserController" class with a basic endpoint

---
+++ src/main/java/com/codecool/happiness_jar/user/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.codecool.happiness_jar.user.controller;
+
+
+import com.codecool.happiness_jar.user.model.User;
+import com.codecool.happiness_jar.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/user/{user_id}")
+    public User getUser(@PathVariable("user_id") int userId) {
+        return userService.getUserById(userId);
+    }
+
+
+
+}
