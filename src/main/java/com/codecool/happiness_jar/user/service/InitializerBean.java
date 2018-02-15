d7d8d563e1f811e94b0da8b4a5a46ab75700970d
ollepeter <mr.peterolle@gmail.com>
2018-02-15 17:32:27 +0100

    Add onemore User

--- src/main/java/com/codecool/happiness_jar/user/service/InitializerBean.java
+++ src/main/java/com/codecool/happiness_jar/user/service/InitializerBean.java
@@ -8,5 +8,7 @@ public class InitializerBean {
     public InitializerBean(UserService userService) {
         User user1 = new User("John", "john@gmail.com", "12345");
         userService.saveUser(user1);
+        User user2 = new User("Tom", "tom@gmail.com", "67890");
+        userService.saveUser(user2);
     }
 }
